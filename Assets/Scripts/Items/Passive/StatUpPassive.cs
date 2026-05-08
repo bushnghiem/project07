047fcd15cf2c4c9cec3a46621e8df843e081db78
Bush Nghiem <bnghiem123@gmail.com>
2026-05-08 12:46:21 -0500

    Organized Folders and added content for Scrapyards

--- Assets/Scripts/Items/Passive/StatUpPassive.cs
+++ Assets/Scripts/Items/Passive/StatUpPassive.cs
@@ -1,27 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class StatAdjustment
+{
+    public ShipStatType statType;
+
+    [Header("Bonuses")]
+    public float flatBonus;
+    public float percentBonus;
+}
 
 [CreateAssetMenu(menuName = "Items/Passive/Stat Up")]
 public class StatUpPassive : PassiveItem
 {
-    [Header("Stat Settings")]
-    public ShipStatType statType;
-    public float statChangeAmount;
-
-    private StatModifier createdModifier;
+    [Header("Stat Adjustments")]
+    public List<StatAdjustment> statAdjustments = new();
 
     public override void ApplyEffect(Unit unit)
     {
-        if (unit is UnitBase unitBase)
+        if (unit is not UnitBase unitBase)
+            return;
+
+        foreach (var adjustment in statAdjustments)
         {
-            createdModifier = new StatModifier
+            StatModifier modifier = new StatModifier
             {
-                statType = statType,
-                flatBonus = statChangeAmount,
-                percentBonus = 0f,
+                statType = adjustment.statType,
+                flatBonus = adjustment.flatBonus,
+                percentBonus = adjustment.percentBonus,
                 sourceID = itemID
             };
 
-            unitBase.AddStatModifier(createdModifier);
+            unitBase.AddStatModifier(modifier);
         }
     }
 
